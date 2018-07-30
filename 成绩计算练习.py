59c99600854f14f680020d00ef87b24752133a34
l1395851238 <1395851238@qq.com>
2018-07-30 15:52:50 +0800

    成绩计算练习.py

---
+++ 成绩计算练习.py
@@ -0,0 +1,14 @@
+scoer = int(input())
+
+if scoer <= 100:
+    if  scoer >= 90:
+        print("A")
+    elif scoer >= 80:
+        print("B")
+    elif scoer >= 70:
+        print("C")
+    elif scoer >= 60:
+        print("D")
+    elif scoer >= 0:
+        print("不及格")
+
